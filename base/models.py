53d4c2dfc86331fbb66dd3d2540f71ecb49724b3
khajira <khajirachris@gmail.com>
2024-03-12 22:33:38 -0400

    Add the Topic class for the model

--- base/models.py
+++ base/models.py
@@ -2,6 +2,13 @@ from django.db import models
 
 # Create your models here.
 # models.Models inherits from the django models class
+
+class Topic(models.Modle):
+    name = models.CharField(max_length=200)
+    def __str__(self):
+        return self.name
+
+
 class Room(models.Model):
     # host =
     # topic = 
