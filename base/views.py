41a4589b585be84cfa8ccf57ca6771cb1e3a7b30
khajira <khajirachris@gmail.com>
2024-03-14 19:06:36 -0400

    Using the Django Forms to do a post request

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Room
+from .forms import RoomForm
 # Create your views here.
 
 # rooms = [
@@ -24,4 +25,11 @@ def room(request,pk):
     room = Room.objects.get(id=pk)
     context = {'room': room}
     return render(request,'base/room.html', context)
-    
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        print(request.POST)
+        
+    context = {'form':form}
+    return render(request,'base/room_form.html', context)
