458a01c6a19e2de9132708c9740276e4ef578cc4
khajira <khajirachris@gmail.com>
2024-03-14 20:04:52 -0400

    Added meta class to order the results in descending order

--- base/models.py
+++ base/models.py
@@ -22,6 +22,8 @@ class Room(models.Model):
     #auto_now_add will only take a snapshot once when the record is created
     created = models.DateTimeField(auto_now_add = True)
 
+    class Meta:
+        ordering = ['-updated','-created']
     def __str__(self):
         return self.name
     
